8c83c412609373ad226decbff15bcb342752f14f
Roger Vieira <rogerdenis@gmail.com>
2022-06-04 15:07:39 -0300

    added consumer initial structure

---
+++ MessageServiceConsumer/src/main/java/rogerdenisvieira/messageservice/consumer/MessageServiceConsumer/MessageServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package rogerdenisvieira.messageservice.consumer.MessageServiceConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageServiceConsumerApplication.class, args);
+	}
+
+}
