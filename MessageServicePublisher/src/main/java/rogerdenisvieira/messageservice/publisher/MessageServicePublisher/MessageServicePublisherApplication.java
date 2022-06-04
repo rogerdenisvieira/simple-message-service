ecfb4cc3e11fa105156603d3ecf1796889bf28a6
Roger Vieira <rogerdenis@gmail.com>
2022-06-04 15:04:43 -0300

    added publisher initial structure

---
+++ MessageServicePublisher/src/main/java/rogerdenisvieira/messageservice/publisher/MessageServicePublisher/MessageServicePublisherApplication.java
@@ -0,0 +1,13 @@
+package rogerdenisvieira.messageservice.publisher.MessageServicePublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServicePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageServicePublisherApplication.class, args);
+	}
+
+}
---
+++ MessageServicePublisher/src/test/java/rogerdenisvieira/messageservice/publisher/MessageServicePublisher/MessageServicePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package rogerdenisvieira.messageservice.publisher.MessageServicePublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServicePublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
